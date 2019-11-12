3152a227ff73786485dee9ede2627f1a78da09ff
HajiraMasood <masood.hajira@gmail.com>
2019-11-12 23:13:39 +0200

    changes needed for heroku deployment

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const DbPlugin = require("./server/db");
 
 const init = async () => {
   const server = Hapi.server({
-    port: 3001,
+    port: process.env.PORT || 3001,
     host: "0.0.0.0",
     routes: {
       cors: true

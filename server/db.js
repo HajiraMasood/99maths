677611b0e9cbc617fe8e837f0be1ab1fff1ab8f3
HajiraMasood <masood.hajira@gmail.com>
2019-11-12 23:16:10 +0200

    use the 99math db

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 const MongoClient = require("mongodb").MongoClient;
 const uri =
-  "mongodb+srv://admin:admin@cluster0-x5giy.mongodb.net/test?retryWrites=true&w=majority";
+  "mongodb+srv://admin:admin@cluster0-x5giy.mongodb.net/99math?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
 exports.plugin = {

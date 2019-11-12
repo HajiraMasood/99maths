0e35abda7339097ce8a0a949f6180b67bd082c42
HajiraMasood <masood.hajira@gmail.com>
2019-11-12 19:31:11 +0200

    basic

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class List extends React.Component {
+  showList(list) {
+    const notes = [...list];
+    const listItems = notes.map(note => {
+      return <li>{note}</li>;
+    });
+    return <ul>{listItems}</ul>;
+  }
+
+  render() {
+    return <div>{this.showList(this.props.value)}</div>;
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      noteText: "",
+      list: []
+    };
+  }
+
+  updateNoteText(event) {
+    const inputText = event.target.value;
+    this.setState({ noteText: inputText });
+    console.log(this.state.noteText);
+  }
+
+  saveNote() {
+    const savedList = [...this.state.list, this.state.noteText];
+    this.setState({ list: savedList });
+    console.log(this.state.list);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h3>Notes Keep</h3>
+        </header>
+        <body>
+          <div>
+            <input
+              className="note"
+              type="textarea"
+              value={this.state.noteText}
+              onChange={e => this.updateNoteText(e)}
+            ></input>
+            <button onClick={() => this.saveNote()}>Save</button>
+          </div>
+          <div>
+            <List value={this.state.list} />
+          </div>
+        </body>
+      </div>
+    );
+  }
 }
 
 export default App;
